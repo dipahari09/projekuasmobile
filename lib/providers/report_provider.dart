caf114bb4ff0606d1bee985a99b50b78913068f9
dipahari09 <divahari9998@gmail.com>
2025-12-11 17:35:30 +0700

    update

--- lib/providers/report_provider.dart
+++ lib/providers/report_provider.dart
@@ -7,37 +7,31 @@ class ReportNotifier extends StateNotifier<List<ReportModel>> {
     loadReports();
   }
 
-  // ✅ Load semua laporan dari database
   Future<void> loadReports() async {
     final data = await DBHelper.getReports();
     state = data;
   }
 
-  // ✅ Insert laporan baru (Mahasiswa 2)
   Future<void> addReport(ReportModel report) async {
     await DBHelper.insertReport(report);
-    await loadReports(); // refresh otomatis
+    await loadReports();
   }
 
-  // ✅ Update laporan (Mahasiswa 4)
   Future<void> updateReport(ReportModel report) async {
     await DBHelper.updateReport(report);
     await loadReports();
   }
 
-  // ✅ Delete laporan (Mahasiswa 4)
   Future<void> deleteReport(int id) async {
     await DBHelper.deleteReport(id);
     await loadReports();
   }
 
-  // ✅ Refresh manual (dipakai setelah edit)
   Future<void> refresh() async {
     await loadReports();
   }
 }
 
-// ✅ Provider utama yang dipakai Mahasiswa 2, 3, dan 4
 final reportProvider =
 StateNotifierProvider<ReportNotifier, List<ReportModel>>(
       (ref) => ReportNotifier(),
